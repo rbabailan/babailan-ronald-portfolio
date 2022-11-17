259975234b3c4ebde79589ba643d2895c3a0a122
Ronald-Bn <ronaldbabailan0026@gmail.com>
2022-11-18 07:24:27 +0800

    fixing responsive layout

--- src/content/project/projectsList.js
+++ src/content/project/projectsList.js
@@ -2,16 +2,18 @@ import style from './style.module.css';
 
 function ProjectsList(props) {
     return (
+
         <div className={style.projectsList}>
-            <img alt={"images"} src={props.img} className={style.img}></img>
+            <div className={style.primaryImg}>
+                <img alt={"images"} src={props.img} className={style.img}></img>
+            </div>
             <div className={style.description}>
                 <h1 className={style.title}>{props.title}</h1>
-
                 <p className={style.paragraph}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero est aliquam inventore, maxime quae repellat labore laboriosam nemo totam consequuntur provident, sed placeat perspiciatis doloremque temporibus voluptatum fugiat vel iusto.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae fuga molestiae blanditiis iusto ad dicta nostrum velit. Veniam nam, dolorum, necessitatibus, ab iusto cupiditate temporibus quos sunt a eveniet hic!</p>
                 {props.children}
             </div>
-        </div>
+        </div >
     );
 }
 
