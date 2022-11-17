bf2a9e4b41a27c784f8607183ed94ce01b6a8b5a
Ronald-Bn <ronaldbabailan0026@gmail.com>
2022-11-18 04:28:55 +0800

    adding responsiveness

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import style from "./Navbar.module.css";
+import menu from "../img/menu.png";
+
 
 function Navbar() {
+    const [toggleNav, setToggleNav] = useState(false);
+    const [checkWidth, setCheckWidth] = useState(window.innerWidth);
+    // const isActive = () => {
+    //     setActive(true);
+    //     console.log("set!");
+    // }
+
+    const checkFunc = () => {
+        console.log(checkWidth);
+        setCheckWidth(window.innerWidth);
+    };
+
+    const toggleNavFunc = () => {
+        setToggleNav(!toggleNav);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", checkFunc);
+    });
+
     return (
-        <div className={style.navbarDiv}>
-            <div>
+        <>
+            {
+                checkWidth < 768 &&
+                <div style={{ position: "absolute", right: 0 }}>
+                    <button className={style.menuButton} onClick={toggleNavFunc}>
+                        <img clasName={style.menuIcon} src={menu} alt="menu-icon" width="36px" />
+                    </button>
+                </div>
+            }
+            <div className={toggleNav ? style.navbarDivMobile : style.navbarDiv}>
+                {
+                    checkWidth < 768 &&
+                    <div>
+                        <button onClick={toggleNavFunc}><img src={menu} width="36px" /></button>
+                    </div>
+                }
                 <nav>
                     <ul>
                         <li>
@@ -19,8 +55,8 @@ function Navbar() {
                         </li>
                     </ul>
                 </nav>
-            </div>
-        </div>
+            </div >
+        </>
     );
 }
 
--- src/content/Home/Home.js
+++ src/content/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Home.module.css";
 import { motion } from 'framer-motion';
 import Navbar from "../../component/Navbar";
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 function Home() {
     const animations = {
         initial: { opacity: 0, x: -100 },
-        animate: { opacity: 1, x: 0 }
+        animate: { opacity: 1, x: 0 },
     }
 
     return (
@@ -27,7 +27,6 @@ function Home() {
                     <h4 className={style.homeInfoMe}>RONALD BABAILAN</h4>
                     <h4 className={style.homeInfoWork}>Web Developer</h4>
                     <p className={style.homeInfoP}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel imperdiet risus. Proin consequat diam et lacus luctus, id pellentesque augue elementum. Vivamus congue tellus nec nunc scelerisque, ac bibendum nibh pellentesque. Ut euismod erat lectus, nec suscipit velit condimentum eu. Sed neque nunc, malesuada id vehicula eu, euismod id mi.
                     </p>
                     <button className={style.pickMeButton}><Link to="/About">Explore</Link></button>
                 </motion.div>
